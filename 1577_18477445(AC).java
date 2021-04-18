02d0ecaa552a7e947f3afd464611b4943fd659a3
moominsoo <sooyeon266@naver.com>
2021-04-18 21:32:12 +0900

    CodeUp

---
+++ 1577_18477445(AC).java
@@ -0,0 +1,18 @@
+﻿import java.util.Scanner;
+public class Main {
+    public static int myabs(int n) {
+        if (n>=0){
+            return n;
+        }
+        else {
+            return -n;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int result = myabs(n);
+        System.out.println(result);
+    }
+}
+
