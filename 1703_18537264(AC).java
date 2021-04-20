77427f9abbc323bde4f80c71ee9270c4156325f6
moominsoo <sooyeon266@naver.com>
2021-04-20 22:48:28 +0900

    CodeUp

---
+++ 1703_18537264(AC).java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int minute = (n/60)%60;
+        int second = n%60;
+        int hour = (n/60)/60;
+
+        System.out.print(hour+" ");
+        System.out.print(minute+" ");
+        System.out.print(second);
+    }
+}
+
