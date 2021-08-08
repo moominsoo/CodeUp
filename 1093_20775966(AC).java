777f0c1c75ed270081d4da6f706ee53e67759d6b
moominsoo <sooyeon266@naver.com>
2021-08-08 21:40:35 +0900

    Code Up

---
+++ 1093_20775966(AC).java
@@ -0,0 +1,25 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        int[] arr = new int[24];
+        int[] call = new int[num];
+
+        for(int i=0; i<num; i++){
+            call[i] = scan.nextInt();
+        }
+        for(int i=0; i<num; i++){
+            arr[call[i]]++;
+        }
+
+        for(int i=1; i<24; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+};
+
